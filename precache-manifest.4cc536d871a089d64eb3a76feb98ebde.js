3492c72ecde10cebfffc3ed904a2f64a936e1e68
Code Sandbox <hello@codesandbox.io>
2021-12-31 06:23:37 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4cc536d871a089d64eb3a76feb98ebde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fefbc6c734d3f17bc72ac22260bd866",
+    "url": "https://ramalingaa.github.io/csb-7qy4c/index.html"
+  },
+  {
+    "revision": "088b521f3df7dc3a7beb",
+    "url": "https://ramalingaa.github.io/csb-7qy4c/static/css/main.ec89a2b7.chunk.css"
+  },
+  {
+    "revision": "2043848d3e431b664b6c",
+    "url": "https://ramalingaa.github.io/csb-7qy4c/static/js/2.8ba1def2.chunk.js"
+  },
+  {
+    "revision": "088b521f3df7dc3a7beb",
+    "url": "https://ramalingaa.github.io/csb-7qy4c/static/js/main.a500a48b.chunk.js"
+  },
+  {
+    "revision": "629e0877d8a1a7b2be3e",
+    "url": "https://ramalingaa.github.io/csb-7qy4c/static/js/runtime~main.cb58cedf.js"
+  }
+]);
